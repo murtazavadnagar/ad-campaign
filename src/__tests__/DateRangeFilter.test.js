4614dd43151c664b952931a90e80f3fa425ab11d
murtazavadnagar <vadnagarwalamurtaza@gmail.com>
2025-02-24 05:10:53 +0530

    code clean up

--- src/__tests__/DateRangeFilter.test.js
+++ src/__tests__/DateRangeFilter.test.js
@@ -1,37 +1,13 @@
 import React from "react";
-import * as redux from "react-redux";
 import "@testing-library/jest-dom";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import campaignReducer, { setFilterDateRange } from "../redux/campaignSlice";
+import { screen } from "@testing-library/react";
 import DateRangeFilter from "../components/DateRangeFilter";
 import { renderWithProviders } from "../utils/components/test-utils";
 
 describe("DateRangeFilter Component", () => {
-  // let store;
-  let dispatchMock;
-
-  beforeEach(() => {
-    // store = configureStore({
-    //   reducer: {
-    //     campaigns: campaignReducer,
-    //   },
-    // });
-
-    dispatchMock = jest.fn();
-    // jest.spyOn(redux, "useDispatch").mockReturnValue(dispatchMock);
-  });
-
   it("should allow selecting a start date", () => {
     renderWithProviders(<DateRangeFilter />);
 
-    // const startDateInput = screen.getByText(/Start Date/i);
     expect(screen.getByText(/Start Date/i)).toBeInTheDocument();
-    // fireEvent.change(startDateInput, { target: { value: "2024-02-01" } });
-
-    // expect(dispatchMock).toHaveBeenCalledWith(
-    //   setFilterDateRange({ start: "2024-02-01", end: "" })
-    // );
   });
 });
--- src/__tests__/SearchFilter.test.js
+++ src/__tests__/SearchFilter.test.js
@@ -1,57 +1,13 @@
-/**
- * @vitest-environment jsdom
- */
 import React from "react";
-import * as redux from "react-redux";
 import "@testing-library/jest-dom";
-import { screen, fireEvent } from "@testing-library/react";
-import { setFilterText } from "../redux/campaignSlice";
+import { screen } from "@testing-library/react";
 import SearchFilter from "../components/SearchFilter";
 import { renderWithProviders } from "../utils/components/test-utils";
 
 describe("SearchFilter Component", () => {
-  let dispatchMock;
-
-  beforeEach(() => {
-    dispatchMock = jest.fn();
-    // jest.spyOn(redux, "useDispatch").mockReturnValue(dispatchMock);
-  });
-
   it("should render the search input", () => {
-    renderWithProviders(
-        <SearchFilter />
-    );
+    renderWithProviders(<SearchFilter />);
 
     expect(screen.getByPlaceholderText("Search Campaign")).toBeInTheDocument();
   });
-
-//   it("should update Redux state when typing in search box", () => {
-//     renderWithProviders(
-//         <SearchFilter />
-//     );
-
-//     const searchInput = screen.getByPlaceholderText("Search Campaign");
-
-//     fireEvent.change(searchInput, { target: { value: "Test Campaign" } });
-
-//     expect(dispatchMock).toHaveBeenCalledWith(setFilterText("Test Campaign"));
-//   });
-
-//   it("should clear input and reset Redux state when clicking clear button", () => {
-//     renderWithProviders(
-//         <SearchFilter />
-//     );
-
-//     const searchInput = screen.getByPlaceholderText("Search Campaign");
-
-//     // Type in search box
-//     fireEvent.change(searchInput, { target: { value: "New Search" } });
-//     expect(dispatchMock).toHaveBeenCalledWith(setFilterText("New Search"));
-
-//     // Click clear button
-//     const clearButton = screen.getByRole("button");
-//     fireEvent.click(clearButton);
-
-//     expect(dispatchMock).toHaveBeenCalledWith(setFilterText(""));
-//   });
 });
